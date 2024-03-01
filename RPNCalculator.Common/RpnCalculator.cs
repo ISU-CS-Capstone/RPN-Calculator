6f94358c52c5f2c7d058a11f64eabc9ad58368f1
Kolby Christiansen <kolbyjayce1@gmail.com>
2024-02-29 23:49:48 -0700

    Mobile working

---
+++ RPNCalculator.Common/RpnCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPNCalculator.Common
+{
+    public class RpnCalculator
+    {
+        public static RpnCalculator CreateInstance()
+        {
+            return new RpnCalculator();
+        }
+    }
+}
