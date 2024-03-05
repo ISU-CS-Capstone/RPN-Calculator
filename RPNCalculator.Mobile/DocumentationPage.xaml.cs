b3aa2bd26d4fc9442d39720e09e0651c09faa84a
Kolby Christiansen <kolbyjayce1@gmail.com>
2024-03-04 18:09:50 -0700

    testing to see if fixes

--- RPNCalculator.Mobile/DocumentationPage.xaml.cs
+++ RPNCalculator.Mobile/DocumentationPage.xaml.cs
@@ -28,7 +28,7 @@ public partial class DocumentationPage : ContentPage
             var markdown = await reader.ReadToEndAsync();
             Console.WriteLine(markdown);
             var html = Markdown.ToHtml(markdown);
-            DocWebView.Source = new HtmlWebViewSource { Html = html };
+            //DocWebView.Source = new HtmlWebViewSource { Html = html };
         }
     }
 }
--- RPNCalculator.Mobile/MainPage.xaml.cs
+++ RPNCalculator.Mobile/MainPage.xaml.cs
@@ -7,18 +7,6 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
-
     private async void OnDocumentationLinkClicked(object sender, EventArgs e)
     {
         // Navigate to the DocumentationPage
