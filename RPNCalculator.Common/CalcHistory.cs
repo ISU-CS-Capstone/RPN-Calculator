d29550c65b3606912b41d9cc4ab4f8de7e2b58e7
Craig Price <craigprice00@gmail.com>
2024-04-04 20:44:17 -0600

    added basic history and undo handling

---
+++ RPNCalculator.Common/CalcHistory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPNCalculator.Common
+{
+    internal class CalcHistory
+    {
+        List<CalcStatus> History;
+        int historySize = 10;
+
+        public CalcHistory()
+        {
+            History = new List<CalcStatus>();
+        }
+
+        public void updateHistory(CalcStatus x)
+        {
+            History.Add(x);
+            if (History.Count > historySize)
+            {
+                History.RemoveAt(0);
+            }
+        }
+
+        public CalcStatus? getHistory()
+        {
+            //return the last object pushed onto the stack and remove it
+            if (History.Count > 0)
+            {
+                CalcStatus returnVal = History[History.Count - 1];
+                History.RemoveAt(History.Count - 1);
+                return returnVal;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
