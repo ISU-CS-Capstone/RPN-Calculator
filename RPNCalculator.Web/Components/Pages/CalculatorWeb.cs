9a4a08bfe28fe62202e4fcad25f52c6ff3e0be78
Trey <b.treycranney@isu.edu>
2024-04-29 12:26:00 -0600

    Comments
    Adding comments

--- RPNCalculator.Web/Components/Pages/CalculatorWeb.cs
+++ RPNCalculator.Web/Components/Pages/CalculatorWeb.cs
@@ -3,6 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+
+/* This is the original backend that I had created for the web version of the calculator. We ended up deciding to use a common back end for web, mobile, and desktop.
+ * `Brek Cranney */
+
 public class CalculatorWeb
 {
     private Stack<decimal> stack;
@@ -13,12 +17,14 @@ public class CalculatorWeb
     }
 
     // Method to push a number onto the stack
+    // Brek Cranney
     public void Push(decimal number)
     {
         stack.Push(number);
     }
 
     // Method to pop a number from the stack
+    // Brek Cranney
     public decimal Pop()
     {
         if (stack.Count == 0)
@@ -29,6 +35,7 @@ public class CalculatorWeb
     }
 
     // Method to perform addition
+    // Brek Cranney
     public void Add()
     {
         decimal num2 = Pop();
@@ -38,6 +45,7 @@ public class CalculatorWeb
     }
 
     // Method to perform subtraction
+    // Brek Cranney
     public void Subtract()
     {
         decimal num2 = Pop();
@@ -47,6 +55,7 @@ public class CalculatorWeb
     }
 
     // Method to perform multiplication
+    // Brek Cranney
     public void Multiply()
     {
         decimal num2 = Pop();
@@ -56,6 +65,7 @@ public class CalculatorWeb
     }
 
     // Method to perform division
+    // Brek Cranney
     public void Divide()
     {
         decimal num2 = Pop();
@@ -69,12 +79,14 @@ public class CalculatorWeb
     }
 
     // Method to clear the stack
+    // Brek Cranney
     public void Clear()
     {
         stack.Clear();
     }
 
     // Method to get the current stack content
+    // Brek Cranney
     public string GetStackContent()
     {
         return string.Join(" ", stack.Reverse());
